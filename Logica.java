f97b8f74326414734ae76bf60b1a284d57962e9b
Gabriel Neugebauer <gabrielneugebauer@hotmail.com>
2025-06-20 22:31:36 -0300

    Notificações completo

--- Logica.java
+++ Logica.java
@@ -1,6 +1,6 @@
 public class Logica {
     public static void main(String[] args) {
-        int prioridade = 4;
+        /*int prioridade = 4;
         int urgencia = 8;
         if (prioridade > 3 && urgencia > 7) {
             System.out.println("Tarefa crítica");
@@ -8,6 +8,16 @@ public class Logica {
             System.out.println("Tarefa importante");
         } else {
             System.out.println("Tarefa comum");
+        } */
+
+        int notificacoes = 0;
+        for (int i = 0; i <= 5; i++) {
+            if (i % 2 == 0) {
+                notificacoes+=2;
+            } else {
+                notificacoes++;
+            }
         }
+        System.out.println("Notificações: " + notificacoes);
     }
 }
